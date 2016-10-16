1134354a1584bafb970020f0f87d02dcbd99113b
KINAN WHYTE <kinanwhyte@gmail.com>
2016-10-16 15:34:07 -0600

    adds a few polishes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -6,7 +6,7 @@ export default class App extends React.Component {
   render () {
     return (
       <div>
-        <h1 id="greeting">Welcome to Weathrly</h1>
+        <h1 id="greeting">Welcome to <span>Weathrly</span></h1>
         <h3 id="location-request">Enter or Change your Location for Weathrly Conditions!</h3>
         <WeatherForecast />
       </div>
